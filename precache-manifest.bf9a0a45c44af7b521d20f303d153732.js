a59104a7a8d4101025117405803103aad7cb08ef
Eric Na <hyunwoo.na@gmail.com>
2020-05-26 20:53:03 -0700

    Updates

---
+++ precache-manifest.bf9a0a45c44af7b521d20f303d153732.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5df8b72bf5baec80e5d61e4357ae96",
+    "url": "/ax1-cards/index.html"
+  },
+  {
+    "revision": "67a23ea72a9ea022453a",
+    "url": "/ax1-cards/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "484f2d1bc7d1bc27beec",
+    "url": "/ax1-cards/static/js/2.ded6475c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ax1-cards/static/js/2.ded6475c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a23ea72a9ea022453a",
+    "url": "/ax1-cards/static/js/main.02dc6187.chunk.js"
+  },
+  {
+    "revision": "0f82987b7f77e0411fa1",
+    "url": "/ax1-cards/static/js/runtime-main.07c5a73f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ax1-cards/static/media/logo.5d5d9eef.svg"
+  }
+]);
